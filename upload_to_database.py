dc0ec156e4cdeb749b8a2e9274c159db5df7ee2f
stalincrisanto <stalincrisanto@hotmail.com>
2024-10-10 17:00:33 -0500

    feat: upload data to db

--- upload_to_database.py
+++ upload_to_database.py
@@ -1,14 +1,14 @@
 import pandas as pd
-import psycopg2
 import os
+import psycopg2
 import time
 
 # Configuración de la conexión a PostgreSQL
 conexion = psycopg2.connect(
-    host="localhost",  # Cambia esto por tu host de PostgreSQL
-    database="postgres",  # Nombre de la base de datos
-    user="postgres",  # Usuario de PostgreSQL
-    password="301218",  # Contraseña de PostgreSQL
+    host="tuservidor",  # Cambia esto por tu host de PostgreSQL
+    database="tubasedatos",  # Nombre de la base de datos
+    user="tuusuario",  # Usuario de PostgreSQL
+    password="tucontraseña",  # Contraseña de PostgreSQL
 )
 
 
@@ -18,7 +18,7 @@ def insertar_csv_postgresql(conexion, archivo_csv, tabla):
     with conexion.cursor() as cursor:
         with open(archivo_csv, "r", encoding="latin1") as f:
             cursor.copy_expert(
-                f"COPY {tabla} (id, concession_code, chs_data_dat, account_fiscal_id, ruta_xml_enviado, ruta_xml_recibido, ruta_xml_comprobante_recibido, ruta_pdf_generado) FROM STDIN WITH CSV HEADER DELIMITER ','",
+                f"COPY {tabla} (id, dateDocModificado) FROM STDIN WITH CSV HEADER DELIMITER ','",
                 f,
             )
         conexion.commit()
@@ -29,32 +29,23 @@ def insertar_csv_postgresql(conexion, archivo_csv, tabla):
     )
 
 
-# Cargar el CSV
+# Cargar el CSV original
 file_path = r"C:\Users\stalin.crisanto\Desktop\srimvmn_202406.csv"
 df = pd.read_csv(file_path, encoding="latin1", sep=";", low_memory=False)
 
 # Selecciona las columnas que quieres mantener
-columnas_a_conservar = [
-    "id",
-    "concession_code",
-    "chs_data_dat",
-    "account_fiscal_id",
-    "ruta_xml_enviado",
-    "ruta_xml_recibido",
-    "ruta_xml_comprobante_recibido",
-    "ruta_pdf_generado",
-]
+columnas_a_conservar = ["id", "dateDocModificado"]
 df_filtrado = df[columnas_a_conservar]
 
 # Definir el número de registros por archivo temporal
-registros_por_archivo = 500000
+registros_por_archivo = 500000  # Puedes ajustar este valor
 
 # Carpeta para almacenar los archivos temporales
 carpeta_temporales = "archivos_temporales"
 os.makedirs(carpeta_temporales, exist_ok=True)
 
 # Nombre de la tabla en PostgreSQL
-tabla_destino = "csv_srimvmn"
+tabla_destino = "nombre_de_tu_tabla"
 
 # Medir el tiempo total del proceso
 tiempo_inicio_total = time.time()
@@ -78,6 +69,3 @@ print(f"Proceso completado. Tiempo total de inserción: {tiempo_total:.2f} segun
 
 # Cerrar la conexión a la base de datos
 conexion.close()
-
-# # Guardar el DataFrame filtrado en un nuevo archivo CSV
-# df_filtrado.to_csv('archivo_filtrado.csv', index=False)
